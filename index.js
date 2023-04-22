28a6cb51e33555477e38742f38199272b66f48fa
carlos <carlosleonfranco13@gmail.com>
2023-04-21 21:15:34 -0500

    Layout principal

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const app = express();
 // Definir puerto
 const port = process.env.PORT || 4000;
 
+// Habilitar PUG
+app.set('view engine', 'pug');
+
+// Definir la carpeta publica
+app.use(express.static('public'));
+
 // Agregar router
 app.use('/', router);
 
--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,10 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => { // req - Lo que enviamos : res - Lo que express nos responde
-    res.send('Inicio');
+    res.render('inicio');
 });
-router.get('/nosotros', (req, res) => { // req - Lo que enviamos : res - Lo que express nos responde
-    res.send('Nosotros');
-});
-router.get('/contacto', (req, res) => { // req - Lo que enviamos : res - Lo que express nos responde
-    res.send('Contacto');
+router.get('/nosotros', (req, res) => { // req - Lo que enviamos : res - Lo que express nos responde    
+    res.render('nosotros');
 });
 
 export default router;
