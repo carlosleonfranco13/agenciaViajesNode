1d6f7fb74eec99423c7dd5cb82a7fa9f6309ab03
carlos <carlosleonfranco13@gmail.com>
2023-04-23 19:31:58 -0500

    Creando 'Nosotros'

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,27 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => { // req - Lo que enviamos : res - Lo que express nos responde
-    res.render('inicio');
+   
+    res.render('inicio', {
+        pagina: 'Inicio'
+    });
 });
-router.get('/nosotros', (req, res) => { // req - Lo que enviamos : res - Lo que express nos responde    
-    res.render('nosotros');
+router.get('/nosotros', (req, res) => {
+    res.render('nosotros', {
+        pagina: 'Nosotros'
+    });
+});
+
+router.get('/viajes', (req, res) => {
+    res.render('viajes', {
+        pagina: 'Viajes'
+    });
+});
+
+router.get('/testimoniales', (req, res) => {
+    res.render('testimoniales', {
+        pagina: 'Testimoniales'
+    });
 });
 
 export default router;
